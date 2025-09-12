317b2ff6670fa078fd190774f5d799304d8f46e4
ztamagoru <zdamaria119@gmail.com>
2025-09-12 12:46:13 -0300

    added flashlight

--- scenes/components/player/player.gd
+++ scenes/components/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 @export var animation_player : AnimationPlayer
 
+@export var flashlight : SpotLight3D
+
 const speed : float = 5.0
 const sprint_multiplier : float = 3.0
 const crouch_speed : float = 4.0
@@ -14,7 +16,13 @@ const jump_force : float = 3.5
 const jump_velocity : float = 4.5
 const gravity : float = 8.0
 
+const flashlight_energy : float = 15.0
+
 var _is_crouching : bool = false
+var _is_flashlight_on : bool = false
+
+func _ready() -> void:
+	flashlight.light_energy = 0
 
 func _process(_delta) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
@@ -24,11 +32,23 @@ func _input(event):
 	if event.is_action_pressed("move_crouch"):
 		toggle_crouch()
 	
+	if event.is_action_pressed("flashlight"):
+		toggle_flashlight()
+	
 	if event.is_action_pressed("move_sprint"):
 		current_speed = speed * sprint_multiplier
 	
 	if event.is_action_released("move_sprint"):
 		current_speed = speed
+
+func toggle_flashlight():
+	_is_flashlight_on = !_is_flashlight_on
+	
+	if _is_flashlight_on:
+		flashlight.light_energy = flashlight_energy
+	else:
+		flashlight.light_energy = 0.0
+	
 
 func toggle_crouch():
 	if _is_crouching:
