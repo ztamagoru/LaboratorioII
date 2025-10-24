e2a25fbb824fc6cd9cfeb142182fd3eaf00b4f10
ztamagoru <zdamaria119@gmail.com>
2025-10-24 12:11:26 -0300

    enemy now randomly stops in each crack based on weighted rng

--- scripts/scenes/components/enemies/flashlight enemy/flashlight_enemy.gd
+++ scripts/scenes/components/enemies/flashlight enemy/flashlight_enemy.gd
@@ -1,30 +1,36 @@
 extends PathFollow3D
 
 const speed : float = 5.0
-const stop_time : float = 2.5
 const stop_distance : float = 0.05
 
 var stop_points : Array[Marker3D] = []
 
-var stop_timer : float = 0.0
 var _is_stopped : bool = false
+
+@onready var cd_timer : Timer = $WaitTimer
+
+const Rarity = {
+	"COMMON": 0.7
+	, "UNCOMMON": 0.3
+}
+
+var dict = {
+	"continue": Rarity.COMMON
+	, "stop": Rarity.UNCOMMON
+}
 
 func _ready():
 	for child in $"../StopPoints".get_children():
 		if child is Marker3D:
 			stop_points.append(child)
 	
-	#await get_tree().process_frame`
+	#await get_tree().process_frame
 	
 	Globals.player.flash_area.scare_enemy.connect(continue_path)
 	loop = true
 
 func _physics_process(delta : float):
 	if _is_stopped:
-		#stop_timer -= delta
-		#
-		#if stop_timer <= 0:
-			#_is_stopped = false
 		return
 	
 	progress += speed * delta
@@ -33,11 +39,20 @@ func _physics_process(delta : float):
 		var distance = global_position.distance_to(point.global_position) 
 		
 		if distance <= stop_distance:
-			_is_stopped = true
-			#stop_timer = stop_time
+			if cd_timer.time_left > 0:
+				continue
+			
+			var stopping : bool = weighted_randomness()
+			
+			if stopping:
+				_is_stopped = true
 			break
 
 func continue_path():
-	print("resuming path")
-	
-	_is_stopped = false
+	_is_stopped = false if _is_stopped else true
+	cd_timer.start(3.0)
+
+func weighted_randomness():
+	var key : String = WeightedChoice.pick(dict)
+	print(key)
+	return true if key == "stop" else false
